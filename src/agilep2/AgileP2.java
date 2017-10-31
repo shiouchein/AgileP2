1cddc1e0c2bd55c56d7ba5d4a42b3a50d0e4f787
taruc <taruc@B15>
2017-10-31 11:00:53 +0800

    First Commit ~

--- src/agilep2/AgileP2.java
+++ src/agilep2/AgileP2.java
@@ -15,7 +15,8 @@ public class AgileP2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("First message");
+        System.out.println("First branch");
     }
     
 }
